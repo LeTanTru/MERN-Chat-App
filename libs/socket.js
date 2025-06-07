6d0e5099961086c1a9dc4368c35ed5b30640f1fc
TRU\Tru Le <letantru@gmail.com>
2025-06-07 22:50:03 +0700

    update socket.js

--- libs/socket.js
+++ libs/socket.js
@@ -9,7 +9,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   }
 });
 
